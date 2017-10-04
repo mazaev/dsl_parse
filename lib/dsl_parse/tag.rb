d4ab014681916612c5301241472cf7fc17c58594
Andrey Mazaev <mazaev@list.ru>
2017-10-05 00:23:40 +0700

    interpoint3

--- lib/dsl_parse/tag.rb
+++ lib/dsl_parse/tag.rb
@@ -1,30 +1,32 @@
 module DSLParse
 
   Colors = [:aliceblue, :antiquewhite, :aqua, :aquamarine, :azure, :beige, :bisque, :blanchedalmond, :blue, :blueviolet, :brown, :burlywood, :cadetblue, :chartreuse, :chocolate, :coral, :cornflowerblue, :cornsilk, :crimson, :cyan, :darkblue, :darkcyan, :darkgoldenrod, :darkgray, :darkgreen, :darkkhaki, :darkmagenta, :darkolivegreen, :darkorange, :darkorchid, :darkred, :darksalmon, :darkseagreen, :darkslateblue, :darkslategray, :darkturquoise, :darkviolet, :deeppink, :deepskyblue, :dimgray, :dodgerblue, :firebrick, :floralwhite, :forestgreen, :fuchsia, :gainsboro, :ghostwhite, :gold, :goldenrod, :gray, :green, :greenyellow, :honeydew, :hotpink, :indianred, :indigo, :ivory, :khaki, :lavender, :lavenderblush, :lawngreen, :lemonchiffon, :lightblue, :lightcoral, :lightcyan, :lightgoldenrodyellow, :lightgreen, :lightgrey, :lightpink, :lightsalmon, :lightseagreen, :lightskyblue, :lightslategray, :lightsteelblue, :lightyellow, :lime, :limegreen, :linen, :magenta, :maroon, :mediumaquamarine, :mediumblue, :mediumorchid, :mediumpurple, :mediumseagreen, :mediumslateblue, :mediumspringgreen, :mediumturquoise, :mediumvioletred, :midnightblue, :mintcream, :mistyrose, :moccasin, :navajowhite, :navy, :oldlace, :olive, :olivedrab, :orange, :orangered, :orchid, :palegoldenrod, :palegreen, :paleturquoise, :palevioletred, :papayawhip, :peachpuff, :peru, :pink, :plum, :powderblue, :purple, :red, :rosybrown, :royalblue, :saddlebrown, :salmon, :sandybrown, :seagreen, :seashell, :sienna, :silver, :skyblue, :slateblue, :slategray, :snow, :springgreen, :steelblue, :tan, :teal, :thistle, :tomato, :turquoise, :violet, :wheat, :white, :whitesmoke, :yellow, :yellowgreen]
-
-  ColorsRE = Colors.join( "|" )
+  Languages = [:Afrikaans, :Basque, :Belarusian, :English, :French, :German, :GermanNewSpelling, :Chinese, :ChinesePRC, :Latin, :Polish, :Russian, :Swahili, :Turkish, :Ukrainian, "1033"] # 1033 <~ X3????
+  ColorsRE = /#{ Colors.join( "|" ) }/
+  MarginsRE = /m\d/
+  LanguagesRE = /#{ Languages.join( "|" ) }/
+  DictionariesRE = /\w+ \(..-..\)/
 
   DSLTags = [
-    /\[b\]/,                          /\[\/b\]/,      # :bold
-    /\[i\]/,                          /\[\/i\]/,      # :italic
-    /\[u\]/,                          /\[\/u\]/,      # :underline
-    /\[c\]|\[c (#{ ColorsRE })\]/,    /\[\/c\]/,      # :color
-    /\[\*\]/,                         /\[\/\*\]/,     # :asterisk
-    /\[mX\]/,                         /\[\/m\]/,      # :margin
-    /\[trn\]/,                        /\[\/trn\]/,    # :translation
-    /\[ex\]/,                         /\[\/ex\]/,     # :example
-    /\[com\]/,                        /\[\/com\]/,    # :comment
-    /\[\!trs\]/,                      /\[\/\!trs\]/,  # :noindex
-    /\[s\]/,                          /\[\/s\]/,      # :multimedia
-    /\[url\]/,                        /\[\/url\]/,    # :url
-    /\[p\]/,                          /\[\/p\]/,      # :lable
-    /\[\'\]/,                         /\[\/\'\]/,     # :accent
-    /\[lang\]/,                       /\[\/lang\]/,   # :lamg
-    /\[ref\]/,                        /\[\/ref\]/,    # :link
-    /\[t\]/,                          /\[\/t\]/,      # :transcript
-    /\[sup\]/,                        /\[\/sup\]/,    # :superscript
-    /\[sub\]/,                        /\[\/sub\]/     # :subscript
-  ]
+    /\[b\]/,                              /\[\/b\]/,      # :bold
+    /\[i\]/,                              /\[\/i\]/,      # :italic
+    /\[u\]/,                              /\[\/u\]/,      # :underline
+    /\[c\]|\[c #{ ColorsRE }\]/,          /\[\/c\]/,      # :color
+    /\[\*\]/,                             /\[\/\*\]/,     # :asterisk
+    /\[#{ MarginsRE }\]/,                 /\[\/m\]/,      # :margin
+    /\[trn\]/,                            /\[\/trn\]/,    # :translation
+    /\[ex\]/,                             /\[\/ex\]/,     # :example
+    /\[com\]/,                            /\[\/com\]/,    # :comment
+    /\[\!trs\]/,                          /\[\/\!trs\]/,  # :noindex
+    /\[s\]/,                              /\[\/s\]/,      # :multimedia
+    /\[url\]/,                            /\[\/url\]/,    # :url
+    /\[p\]/,                              /\[\/p\]/,      # :lable
+    /\['\]/,                              /\[\/'\]/,      # :accent
+    /\[lang id=#{ LanguagesRE }\]/,       /\[\/lang\]/,   # :lang
+    /\[ref dict="#{ DictionariesRE }"\]/, /\[\/ref\]/,    # :link
+    /\[t\]/,                              /\[\/t\]/,      # :transcript
+    /\[sup\]/,                            /\[\/sup\]/,    # :superscript
+    /\[sub\]/,                            /\[\/sub\]/]    # :subscript
 
   TagStack = {
     bold:         {},
@@ -41,7 +43,7 @@ module DSLParse
     url:          {},
     lable:        {},
     accent:       {},
-    lamg:         {},
+    lang:         {},
     link:         {},
     transcript:   {},
     superscript:  {},
@@ -52,16 +54,74 @@ module DSLParse
 
   class Tag
 
-    attr :name, :status, :position, :mark
+    attr :name, :status, :position, :raw
 
-    def initialize name, position
-      i = DSLTags.index { |v| v.match name }
+    def initialize dsl_tag, position
+      @raw = dsl_tag
+      i = DSLTags.index { |v| v.match @raw }
       @name     = TagStack.keys[i/2]
       @status   = TagStatus[i%2]
       @position = position
-      @mark     = name.split.last if name
     end
   end
 
+  color_proc = proc { |val| { class: val.match( ColorsRE ).to_s } }
+  margin_proc = proc { |val| { class: val.match( MarginsRE ).to_s } }
+  lang_proc = proc { |val| { lang_id: val.match( LanguagesRE ).to_s } }
+  link_proc = proc { |val| { dict: val.match( DictionariesRE ).to_s } }
+
+  # puts color_proc.call( "[c green]")
+  # puts margin_proc.call( "[m3]")
+  # puts lang_proc.call( "[lang id=1033]" )
+  # puts link_proc.call( "[ref dict=\"LingvoGrammar (En-Ru)\"]" )
+
+  def self.separator str
+    tags, s, tag, is_tag, is_esc = [], "", "", false, false
+    str.each_char do |ch|
+      case ch
+      when "\\"
+        is_esc = true; next        
+      when "["
+        is_tag = true unless is_esc
+        unless is_tag
+          s << ch
+          is_esc = false
+        end
+      when "]"
+        unless is_tag
+          s << ch
+          is_esc = false
+        end
+        is_tag = false unless is_esc
+      else
+        is_tag ? tag << ch : s << ch
+      end
+    end
+    return "#{s}   #{tag}"
+  end
+
+  x = %q{[m1]1) [p]филос.[/p] [trn]схоластика [i][com](средневековая религиозная философия, соединявшая теолого-догматические предпосылки с рационалистической методикой и интересом к формально-логическим проблемам)[/com][/trn][/i][/m]}
+  puts separator( %q([p]сущ.[/p][c];[/c] [p]книжн.[/p]) )
+  puts separator( %q(\[[t]'skəulɪə[/t]\]) )
+  puts separator( x )
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- spec/tag_spec.rb
+++ spec/tag_spec.rb
@@ -2,22 +2,22 @@ require "spec_helper"
 
 RSpec.describe DSLParse::Tag do
 
-  let ( :tag_b_open ) { DSLParse::Tag.new( "[b]", 0 ) }
+  let ( :tag_accent_open ) { DSLParse::Tag.new( "[']", 0 ) }
   let ( :tag_asterisk_close ) { DSLParse::Tag.new( "[/*]", 0 ) }
+  let ( :tag_m1_open ) { DSLParse::Tag.new( "[m1]", 0 ) }
   let ( :tag_c_red_open ) { DSLParse::Tag.new( "[c red]", 0 ) }
+  let ( :tag_lang_latin_open ) { DSLParse::Tag.new( "[lang id=Latin]", 0 ) }
+  let ( :tag_link_open ) { DSLParse::Tag.new( "[ref dict=\"LingvoGrammar (En-Ru)\"]", 0 ) }
 
-  describe "check for '[b]', position: 0" do
-    it "#name: ':bold'" do
-      expect( tag_b_open.name ).to eq :bold
+  describe "check for '[']', position: 0" do
+    it "#name: ':accent'" do
+      expect( tag_accent_open.name ).to eq :accent
     end
     it "#status: ':open'" do
-      expect( tag_b_open.status ).to eq :open
+      expect( tag_accent_open.status ).to eq :open
     end
     it "#position: 0" do
-      expect( tag_b_open.position ).to eq 0
-    end
-    it "#mark: 'nil'" do
-      expect( tag_b_open.mark ).to eq nil
+      expect( tag_accent_open.position ).to eq 0
     end
   end
 
@@ -42,6 +42,42 @@ RSpec.describe DSLParse::Tag do
     end
     it "#position: 0" do
       expect( tag_c_red_open.position ).to eq 0
+    end
+  end
+
+  describe "check for '[m1]', position: 0" do
+    it "#name: ':margin'" do
+      expect( tag_m1_open.name ).to eq :margin
+    end
+    it "#status: ':open'" do
+      expect( tag_m1_open.status ).to eq :open
+    end
+    it "#position: 0" do
+      expect( tag_m1_open.position ).to eq 0
+    end
+  end
+
+  describe "check for '[lang id=Latin]', position: 0" do
+    it "#name: ':lang'" do
+      expect( tag_lang_latin_open.name ).to eq :lang
+    end
+    it "#status: ':open'" do
+      expect( tag_lang_latin_open.status ).to eq :open
+    end
+    it "#position: 0" do
+      expect( tag_lang_latin_open.position ).to eq 0
+    end
+  end
+
+  describe "check for '[ref dict=\"LingvoGrammar (En-Ru)\"]', position: 0" do
+    it "#name: ':link'" do
+      expect( tag_link_open.name ).to eq :link
+    end
+    it "#status: ':open'" do
+      expect( tag_link_open.status ).to eq :open
+    end
+    it "#position: 0" do
+      expect( tag_link_open.position ).to eq 0
     end
   end
 
